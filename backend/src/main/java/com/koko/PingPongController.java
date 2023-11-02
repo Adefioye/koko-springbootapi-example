f90cddc76f336dd0a678795a865b473c4a410fc0
Adefioye <abdulhakeemadefioye@gmail.com>
2023-11-02 18:03:29 -0500

    Add @RestController annotation to PingPongController class

--- backend/src/main/java/com/koko/PingPongController.java
+++ backend/src/main/java/com/koko/PingPongController.java
@@ -1,11 +1,11 @@
 package com.koko;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PingPongController {
-
     record PingPong(String result) {}
-
     @GetMapping("/ping")
     public PingPong getPingPong() {
         return new PingPong("ping");
