3e7765577fec9a28731eea2c8bc2922e39a1be89
Adefioye <abdulhakeemadefioye@gmail.com>
2023-12-05 22:29:03 -0600

    Ensure token is used to access secured endpoint

--- backend/src/main/java/com/koko/jwt/JWTUtil.java
+++ backend/src/main/java/com/koko/jwt/JWTUtil.java
@@ -1,5 +1,6 @@
 package com.koko.jwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -16,9 +17,6 @@ import static java.time.temporal.ChronoUnit.DAYS;
 public class JWTUtil {
 
     private static final String SECRET_KEY = "mySecretKeymySecretKeymySecretKeymySecretKey";
-//    public String issueToken(String subject) {
-//        return issueToken(subject, Map.of());
-//    }
 
     public String issueToken(String subject, String ...scopes) {
         return issueToken(subject, Map.of("scopes", scopes));
@@ -45,6 +43,29 @@ public class JWTUtil {
 
     private Key getSigningKey() {
          return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
 
+    public String getSubject(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    private Claims getClaims(String token) {
+        Claims claims = Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims;
+    }
+
+    public boolean isTokenValid(String jwt, String username) {
+        String subject = getSubject(jwt);
+        return subject.equals(username) && !isTokenExpired(jwt);
+    }
+
+    private boolean isTokenExpired(String jwt) {
+        java.util.Date today = Date.from(Instant.now());
+        return getClaims(jwt).getExpiration().before(today);
     }
 }
--- backend/src/main/java/com/koko/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/koko/security/SecurityFilterChainConfig.java
@@ -1,22 +1,37 @@
 package com.koko.security;
 
+import com.koko.jwt.JWTAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityFilterChainConfig {
 
+    private final AuthenticationProvider authenticationProvider;
+    private final JWTAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityFilterChainConfig(AuthenticationProvider authenticationProvider,
+                                     JWTAuthenticationFilter jwtAuthenticationFilter
+    ) {
+        this.authenticationProvider = authenticationProvider;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeHttpRequests()
                 .requestMatchers(
                         HttpMethod.POST,
@@ -29,7 +44,14 @@ public class SecurityFilterChainConfig {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .csrf().disable();
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                );
 
 
         return http.build();
