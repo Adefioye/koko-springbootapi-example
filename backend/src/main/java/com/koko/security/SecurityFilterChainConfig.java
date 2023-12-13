aa13b5619f2106ba0d67d5ddd46bd700e1b87e06
Adefioye <abdulhakeemadefioye@gmail.com>
2023-12-12 23:13:46 -0600

    Allow /ping to have no authentication

--- backend/src/main/java/com/koko/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/koko/security/SecurityFilterChainConfig.java
@@ -45,6 +45,11 @@ public class SecurityFilterChainConfig {
                         "/api/v1/auth/login"
                 )
                 .permitAll()
+                .requestMatchers(
+                        HttpMethod.GET,
+                        "/ping"
+                )
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
