4b5097642f01e73a91e1958f307a7c1fdfc6a0ce
Adefioye <abdulhakeemadefioye@gmail.com>
2023-12-14 18:43:35 -0600

    Create login UI and add validation for username and password

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,110 @@
+import { useForm } from "react-hook-form";
+import { Button } from "./ui/button";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+  Form,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const formSchema = z.object({
+  username: z.string().min(5).max(30),
+  password: z
+    .string()
+    .regex(new RegExp(".*[A-Z].*"), "Require 1 uppercase character")
+    .regex(new RegExp(".*[a-z].*"), "Require 1 lowercase character")
+    .regex(new RegExp(".*\\d.*"), "Require 1 number")
+    .regex(
+      new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"),
+      "Require 1 special character"
+    )
+    .min(8, "Must be at least 8 characters in length"),
+});
+
+const Login = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    mode: "onBlur",
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center text-white bg-slate-600">
+      <div className="w-3/5 shadow-2xl rounded-2xl bg-slate-500 p-10 md:w-2/5">
+        <p className="font-semibold text-lg mb-3 text-center">
+          Sign in to your account
+        </p>
+        <Form {...form}>
+          <form className="w-full" onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="space-y-3">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem className="">
+                    <FormLabel htmlFor="name" className="text-right">
+                      Username
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        type="text"
+                        id="name"
+                        className="bg-inherit focus-visible:ring-transparent"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage className="text-left" />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem className="">
+                    <FormLabel htmlFor="email" className="text-right">
+                      Password
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        id="password"
+                        className="bg-inherit focus-visible:ring-0"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage className="text-center" />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                disabled={
+                  !(form.formState.isValid && form.formState.isDirty) ||
+                  form.formState.isSubmitting
+                }
+                onClick={() => {}}
+                className="w-full"
+              >
+                Sign in
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import { Toaster } from "./components/ui/toaster.tsx";
+import Login from "./components/Login.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/dashboard",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <Toaster />
-    </BrowserRouter>
+    <RouterProvider router={router} />
+    <Toaster />
   </React.StrictMode>
 );
