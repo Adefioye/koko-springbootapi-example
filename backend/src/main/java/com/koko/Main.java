1e1b4d9dfe3fddd78ff053de7580f9ff65e4ad43
Adefioye <abdulhakeemadefioye@gmail.com>
2024-02-05 11:17:16 -0600

    Made few changes in Main class file

--- backend/src/main/java/com/koko/Main.java
+++ backend/src/main/java/com/koko/Main.java
@@ -4,22 +4,15 @@ import com.github.javafaker.Faker;
 import com.koko.customer.Customer;
 import com.koko.customer.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+
 import java.util.Random;
 
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(Main.class, args);
-        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
-
-//        for (String beanDefinitionName: beanDefinitionNames) {
-//            System.out.println(beanDefinitionName);
-//        }
 
     }
 
@@ -40,26 +33,6 @@ public class Main {
 
     }
 
-
-//    @GetMapping("/greet")
-//    public GreetResponse greet(
-//            @RequestParam(value = "name", required = false) String name) {
-//
-//        String greeting = name == null || name.isBlank() ? "Hello" : "Hello " + name;
-//        return new GreetResponse(
-//                greeting,
-//                List.of("Java", "Javascript", "Python"),
-//                new Person("Koko", 30, 30000)
-//        );
-//    }
-//
-//    record Person(String name, int age, double savings) {};
-//
-//    record GreetResponse(
-//            String greeting,
-//            List<String> favProgLanguages,
-//            Person person
-//    ) {}
 }
 
 
